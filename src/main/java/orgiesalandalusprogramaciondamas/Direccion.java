3c1eade739305b2c887e6861d6eee2c4078564ef
Franprog2024 <frangrabaciondedatos@hotmail.com>
2024-11-10 22:10:28 +0100

    Creado un enumerado llamado direccion que contiene los literales: NORESTE, SURESTE, SUROESTE, NOROESTE
    Creado el atributo cadenaAMostrar.
    
    Creado el constructor con la visibilidad adecuada.
    
    Creado el método toString.

---
+++ src/main/java/orgiesalandalusprogramaciondamas/Direccion.java
@@ -0,0 +1,16 @@
+package orgiesalandalusprogramaciondamas;
+
+public enum Direccion {
+    NORESTE("Noreste"), SURESTE("Sureste"), SUROESTE("Suroeste"), NOROESTE("Noroeste");
+
+    private String cadenaAMostrar;
+
+    private Direccion(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
+}
