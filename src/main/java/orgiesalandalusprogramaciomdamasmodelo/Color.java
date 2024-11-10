e9a1eb1a25181357551af2156577dfa83f653a4d
Franprog2024 <frangrabaciondedatos@hotmail.com>
2024-11-10 23:10:13 +0100

    Crea un enumerado llamado Direccion que contenga los literales: Blanco, negro. Los literales estarán parametrizados y a cada uno le pasaremos la representación en cadena de los mismos: "Noreste", "Sureste", "Noroeste" o "Suroeste". Realiza un commit. Crea el atributo cadenaAMostrar (String). Realiza un commit. Crea el constructor con la visibilidad adecuada, que almacenará en el atributo anterior la cadena que se le pase por parámetro.  Realiza un commit. Crea el método toString que devolverá la representación en forma de cadena del literal. Realiza un commit.

--- src/main/java/orgiesalandalusprogramaciomdamasmodelo/Color.java
+++ src/main/java/orgiesalandalusprogramaciomdamasmodelo/Color.java
@@ -1,4 +1,18 @@
 package orgiesalandalusprogramaciomdamasmodelo;
 
 public enum Color {
+
+    BLANCO("Blanco"),
+    NEGRO("Negro");
+
+    private String cadenaAMostrar;
+
+    private Color(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
 }
