2605d5b97b9483760bc050295a1021c610d1ac94
Franprog2024 <frangrabaciondedatos@hotmail.com>
2024-11-13 18:49:33 +0100

    Se ha creado un enumerado llamado Color que contiene los literales: BLANCO y NEGRO.
    Se ha creado el atributo cadenaAMostrar (String).
    
    Se ha creado el constructor con la visibilidad adecuada.
    
    Se ha creado el método toString.

--- src/main/java/orgiesalandalusprogramaciomdamasmodelo/Color.java
+++ src/main/java/orgiesalandalusprogramaciomdamasmodelo/Color.java
@@ -1,16 +1,17 @@
 package orgiesalandalusprogramaciomdamasmodelo;
 
 public enum Color {
-
     BLANCO("Blanco"),
     NEGRO("Negro");
 
-    private String cadenaAMostrar;
+    private final String cadenaAMostrar;
 
+    // Constructor con la visibilidad adecuada (privada)
     private Color(String cadenaAMostrar) {
         this.cadenaAMostrar = cadenaAMostrar;
     }
 
+    // Metodo toString que devuelve la representación en forma de cadena
     @Override
     public String toString() {
         return cadenaAMostrar;
